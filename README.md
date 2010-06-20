4446ae43edef9b072356452230cf01997311736f
Josh Lubaway <josh.lubaway@gmail.com>
2010-06-19 17:03:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Better Status Bar
+
+I hate how Chrome's status bar doesn't show you the entire url. So here is my
+fix for it.
+
+This userscript will display another status bar in the top left corner of the
+screen.
+
+[click here to install](http://example.com/better_status_bar.user.js "Install Better Status Bar")
+
+## Known Issues
+- The status bar displays on the top left, not the bottom left.
+- The original Chrome status bar is still displayed.
---
+++ better_status_bar.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name        Better Status Bar
+// @namespace   josh.lubaway.com
+// @description Chrome's status bar blows. It doesn't show you the full uri.
+// @include     *
+// ==/UserScript==
+
+var StatusBar = {
+
+  initialize: function() {
+    StatusBar.createStatusBar("I'm a status bar!");
+    StatusBar.hide();
+
+    var links = document.getElementsByTagName('a');
+    for (var i=0; i<links.length; i++) {
+      links[i].addEventListener("mouseover", StatusBar.show);
+      links[i].addEventListener("mouseout", StatusBar.hide);
+    }
+  },
+
+  createStatusBar: function(text) {
+    var element  = document.createElement('div');
+    var textNode = document.createElement('p');
+
+    element.style.position = 'fixed';
+    element.style.top = '0px';
+    element.style.left = '0px';
+    element.style.background = '#f6f6f6';
+    element.style.margin = '0px';
+    element.style['border-bottom'] = '1px solid #cdcdcd';
+    element.style['border-right'] = '1px solid #cdcdcd';
+    element.style['-webkit-border-bottom-right-radius'] = '5px';
+
+    textNode.innerHTML = text;
+    textNode.style.color = '#161616';
+    textNode.style.margin = '2px 4px';
+    textNode.style.fontFamily = 'Arial';
+    textNode.style.fontSize = '11px';
+    textNode.style.textShadow = '1px 1px #fefefe';
+    textNode.style['letter-spacing'] = '1px';
+
+    element.appendChild(textNode);
+    document.body.appendChild(element);
+
+    StatusBar.textNode = textNode;
+    StatusBar.bar      = element;
+  },
+
+  text: function(value) {
+    if (value == 'undefined') {
+      value = window.location;
+    }
+
+    StatusBar.textNode.innerHTML = value;
+  },
+
+  hide: function() {
+    StatusBar.bar.style.display = 'none';
+  },
+
+  show: function(event) {
+    StatusBar.text(event.target.href);
+    StatusBar.bar.style.display = 'block';
+  }
+
+}
+
+StatusBar.initialize();
